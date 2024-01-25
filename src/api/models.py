cfebd7a2952169dc4432e1202cada7a34cd31056
Arcarius41 <145110529+Arcarius41@users.noreply.github.com>
2024-01-25 02:09:53 +0000

    copied over models

--- src/api/models.py
+++ src/api/models.py
@@ -2,18 +2,53 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+Base = declarative_base()
+
 class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    __tablename__ = 'user'
+    id = db.Column(Integer, primary_key=True)
+    username = db.Column(String(250), nullable=False)
+    password = db.Column(String(250), nullable=False)
+    person = db.relationship("Person")
 
-    def __repr__(self):
-        return f'<User {self.email}>'
 
-    def serialize(self):
-        return {
-            "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
-        }
+class Planet(Base):
+    __tablename__ = 'planet'
+    id = db.Column(Integer, primary_key=True)
+    name = db.Column(String(250), nullable=False)
+    climate = db.Column(String(250))
+    terrain = db.Column(String(250))
+    population = db.Column(Integer)
+    favorites = db.relationship("Favorite")
+
+class Character(Base):
+    __tablename__ = 'character'
+    id = db.Column(Integer, primary_key=True)
+    name = db.Column(String(250), nullable=False)
+    height = db.Column(Integer)
+    hair_color = db.Column(String(250))
+    eye_color = db.Column(String(250))
+    gender = db.Column(String(250))
+    favorites = db.relationship("Favorite")
+
+class Starship(Base):
+    __tablename__ = 'starship'
+    id = db.Column(Integer, primary_key=True)
+    name = db.Column(String(250), nullable=False)
+    model = db.Column(String(250))
+    manufacturer = db.Column(String(250))
+    crew = db.Column(Integer)
+    passengers = db.Column(Integer)
+    favorites = db.relationship("Favorite")
+    # Add any additional fields as necessary
+
+class Favorite(Base):
+    __tablename__ = 'favorite'
+    id = db.Column(Integer, primary_key=True)
+    user_id = db.Column(Integer, ForeignKey('user.id'))
+    planet_id = db.Column(Integer, ForeignKey('planet.id'))
+    character_id = db.Column(Integer, ForeignKey('character.id'))
+    starship_id = db.Column(Integer, ForeignKey('starship.id'))
+
+## Draw from SQLAlchemy base
+render_er(Base, 'diagram.png')
--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -1,6 +1,7 @@
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
+			message: null,
 			demo: [
 				{
 					title: "FIRST",
@@ -12,27 +13,25 @@ const getState = ({ getStore, getActions, setStore }) => {
 					background: "white",
 					initial: "white"
 				}
-			],
-			favorites: [],
+			]
 		},
 		actions: {
-			addToFavorites: (name) => {
-				const store = getStore();
-				setStore({favorites: [...store.favorites, name]})
-			},
-			removeFromFavorites: (name) => {
-				const store = getStore();
-				const newFavorites = store.favorites.filter((item) => item !== name);
-				setStore({favorites: newFavorites})
-			},
 			// Use getActions to call a function within a fuction
 			exampleFunction: () => {
 				getActions().changeColor(0, "green");
 			},
-			loadSomeData: () => {
-				/**
-					fetch().then().then(data => setStore({ "foo": data.bar }))
-				*/
+
+			getMessage: async () => {
+				try{
+					// fetching data from the backend
+					const resp = await fetch(process.env.BACKEND_URL + "/api/hello")
+					const data = await resp.json()
+					setStore({ message: data.message })
+					// don't forget to return something, that is how the async resolves
+					return data;
+				}catch(error){
+					console.log("Error loading message from backend", error)
+				}
 			},
 			changeColor: (index, color) => {
 				//get the store
