2408eacbf658cfb19df00dfcd108bcc0c50a3580
Arcarius41 <145110529+Arcarius41@users.noreply.github.com>
2024-01-27 22:02:17 +0000

    need to finish with testing postman

--- src/api/models.py
+++ src/api/models.py
@@ -2,43 +2,41 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-Base = declarative_base()
+
 
 class User(db.Model):
     __tablename__ = 'user'
-    id = db.Column(Integer, primary_key=True)
-    username = db.Column(String(250), nullable=False)
-    password = db.Column(String(250), nullable=False)
-    person = db.relationship("Person")
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(250), nullable=False)
+    password = db.Column(db.String(250), nullable=False)
+    favorites = db.relationship("Favorite")
 
 
 class Planet(db.Model):
     __tablename__ = 'planet'
-    id = db.Column(Integer, primary_key=True)
-    name = db.Column(String(250), nullable=False)
-    climate = db.Column(String(250))
-    terrain = db.Column(String(250))
-    population = db.Column(Integer)
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), nullable=False)
+    climate = db.Column(db.String(250))
+    terrain = db.Column(db.String(250))
+    population = db.Column(db.Integer)
     favorites = db.relationship("Favorite")
 
 class Character(db.Model):
     __tablename__ = 'character'
-    id = db.Column(Integer, primary_key=True)
-    name = db.Column(String(250), nullable=False)
-    height = db.Column(Integer)
-    hair_color = db.Column(String(250))
-    eye_color = db.Column(String(250))
-    gender = db.Column(String(250))
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), nullable=False)
+    height = db.Column(db.Integer)
+    hair_color = db.Column(db.String(250))
+    eye_color = db.Column(db.String(250))
+    gender = db.Column(db.String(250))
     favorites = db.relationship("Favorite")
 
 
-class Favorite(Base):
+class Favorite(db.Model):
     __tablename__ = 'favorite'
-    id = db.Column(Integer, primary_key=True)
-    user_id = db.Column(Integer, ForeignKey('user.id'))
-    planet_id = db.Column(Integer, ForeignKey('planet.id'))
-    character_id = db.Column(Integer, ForeignKey('character.id'))
-    starship_id = db.Column(Integer, ForeignKey('starship.id'))
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    planet_id = db.Column(db.Integer, db.ForeignKey('planet.id'))
+    character_id = db.Column(db.Integer, db.ForeignKey('character.id'))
 
-## Draw from SQLAlchemy base
-render_er(db.Model, 'diagram.png')
+
--- src/api/routes.py
+++ src/api/routes.py
@@ -11,55 +11,110 @@ api = Blueprint('api', __name__)
 # Allow CORS requests to this API
 CORS(api)
 
-api = Blueprint('api', __name__)
 
-# User Routes
-@api_blueprint.route('/users', methods=['GET'])
+
+@api.route('/users', methods=['GET'])
 def get_users():
     users = User.query.all()
     return jsonify([user.to_dict() for user in users]), 200
 
-@api_blueprint.route('/user', methods=['POST'])
+@api.route('/users/', methods=['POST'])
 def create_user():
-    data = request.get.json
-    
+    request_body = request.get_json()
+    user = User(username=request_body["username"], password=request_body["password"])
+    db.session.add(user)
+    db.session.commit()
+    return jsonify(user.to_dict()), 200
 
-@api_blueprint.route('/users/<int:id>', methods=['GET'])
+@api.route('/users/<int:id>', methods=['GET'])
+def get_single_user(id):
+    user = User.query.get(id)
+    return jsonify(user.to_dict()), 200
 
-@api_blueprint.route('/users/<int:id>', methods=['PUT'])
+@api.route('/users/<int:id>', methods=['DELETE'])
+def delete_user(id):
+    user = User.query.get(id)
+    db.session.delete(user)
+    db.session.commit()
+    return jsonify(user.to_dict(),'deleted'), 200
 
-@api_blueprint.route('/users/<int:id>', methods=['DELETE'])
 
-# Planet Routes
-@api_blueprint.route('/planets', methods=['GET'])
 
-@api_blueprint.route('/planet', methods=['POST'])
 
-@api_blueprint.route('/planets/<int:id>', methods=['GET'])
+@api.route('/planets/<int:id>', methods=['GET'])
+def get_single_planet(id):
+    planet = Planet.query.get(id)
+    return jsonify(planet.to_dict()), 200
 
-@api_blueprint.route('/planets/<int:id>', methods=['PUT'])
+@api.route('/planets', methods=['GET'])
+def get_planets():
+    planets = Planet.query.all()
+    return jsonify([planet.to_dict() for planet in planets]), 200
 
-@api_blueprint.route('/planets/<int:id>', methods=['DELETE'])
+@api.route('/planets', methods=['POST'])
+def create_planet():
+    request_body = request.get_json()
+    planet = Planet(name=request_body["name"], climate=request_body["climate"], terrain=request_body["terrain"], population=request_body["population"])
+    db.session.add(planet)
+    db.session.commit()
+    return jsonify(planet.to_dict()), 200
+
+@api.route('/planets/<int:id>', methods=['DELETE'])
+def delete_planet(id):
+    planet = Planet.query.get(id)
+    db.session.delete(planet)
+    db.session.commit()
+    return jsonify(planet.to_dict(),'deleted'), 200
 
 # Character Routes
-@api_blueprint.route('/characters', methods=['GET'])
+@api.route('/characters', methods=['GET'])
+def get_characters():
+    characters = Character.query.all()
+    return jsonify([character.to_dict() for character in characters]), 200
 
-@api_blueprint.route('/character', methods=['POST'])
+@api.route('/character', methods=['POST'])
+def create_character():
+    request_body = request.get_json()
+    character =  Character(name=request_body["name"], height=request_body['height'], hair_color=request_body['hair_color'], eye_color=request_body['eye_color'], gender=request_body['gender'])
+    db.session.add(character)
+    db.session.commit()
+    return jsonify(character.to_dict()), 200
 
-@api_blueprint.route('/character/<int:character_id>', methods=['PUT'])
+@api.route('/character/<int:character_id>', methods=['GET'])
+def get_single_character(character_id):
+    character = Character.query.get(character_id)
+    return jsonify(character.to_dict()), 200
 
-@api_blueprint.route('/character/<int:character_id>', methods=['DELETE'])
+@api.route('/character/<int:character_id>', methods=['DELETE'])
+def delete_character(character_id):
+    character = Character.query.get(character_id)
+    db.session.delete(character)
+    db.session.commit()
+    return jsonify(character.to_dict(),'deleted'), 200
 
 # Favorite Routes
-@api_blueprint.route('/favorites', methods=['GET'])
+@api.route('/favorites', methods=['GET'])
+def get_favorites():
+    favorites = Favorite.query.all()
+    return jsonify([favorite.to_dict() for favorite in favorites]), 200
 
-@api_blueprint.route('/favorite', methods=['POST'])
+@api.route('/favorite', methods=['POST'])
+def create_favorite():
+    request_body = request.get_json()
+    favorite = Favorite(user_id=request_body["user_id"], planet_id=request_body["planet_id"], character_id=request_body["character_id"])
+    db.session.add(favorite)
+    db.session.commit()
+    return jsonify(favorite.to_dict()), 200
 
-@api_blueprint.route('/favorite/<int:favorite_id>', methods=['PUT'])
+@api.route('/favorite/<int:favorite_id>', methods=['GET'])
+def get_single_favorite(favorite_id):
+    favorite = Favorite.query.get(favorite_id)
+    return jsonify(favorite.to_dict()), 200
 
-@api_blueprint.route('/favorite/<int:favorite_id>', methods=['DELETE'])
+@api.route('/favorite/<int:favorite_id>', methods=['DELETE'])
+def delete_favorite(favorite_id):
+    favorite = Favorite.query.get(favorite_id)
+    db.session.delete(favorite)
+    db.session.commit()
+    return jsonify(favorite.to_dict(),'deleted'), 200
 
-
-
-
-    return jsonify(response_body), 200
